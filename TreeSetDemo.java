f6a446c87b13ae357f2f7b354358665d11b18ad0
Abhay <abhaypvt2903@gmail.com>
2025-12-18 18:55:00 +0530

    Add TreeSetDemo File

---
+++ TreeSetDemo.java
@@ -0,0 +1,19 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        
+        Set<Integer> set =  new TreeSet<>();
+
+        set.add(20);
+        set.add(05);
+        set.add(35);
+        set.add(25);
+
+        System.out.println(set);
+
+    }
+    
+}

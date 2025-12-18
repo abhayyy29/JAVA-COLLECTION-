59c255687f9cb2058edc9b0b8df13a52cb60bbbc
Abhay <abhaypvt2903@gmail.com>
2025-12-18 21:58:55 +0530

    Modified Generics

--- Generics.java
+++ Generics.java
@@ -1,15 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
-public class Generics {
-    public static void main(String[] args){
-        List<DataInt> l = new ArrayList<DataInt>();
-            l.add(new DataInt());
-            l.add(new DataInt(7,9));
-            for(int i=0; i<l.size();i++){
-                DataInt ob = l.get(i);
-                System.out.println(ob.a);
-                System.out.println(ob.b);
+public class Generics <e,v> {
+         e id;
+         v name;
+
+         Generics(e id, v name){
+            this.id= id;
+            this.name= name;
+
+         }
+
+            e getId(){
+            return id;
+         }
+
+            v getName() {
+                return name;
             }
+
+
+         public static void main(String[] args){
+
+
+            Generics<Integer,String> g= new Generics<Integer,String>(12, "Abhay");
+            System.out.println( "ID =" + g.getId() +  "Name = " + g.getName()) ;
+
+         }
+          
     }
     
-}
+

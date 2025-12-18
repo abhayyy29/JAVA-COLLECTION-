54b691f6803664aafa3475e0561f863742d51c98
Abhay <abhaypvt2903@gmail.com>
2025-12-18 15:40:21 +0530

    Add more element in list

--- ArrayListDemo2.java
+++ ArrayListDemo2.java
@@ -4,11 +4,20 @@ public class ArrayListDemo2 {
     public static void main(String[] args){
 
         List<Integer> list = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
 
         list.add(10);
         list.add(20);
         list.add(30);
+
+        list2.add(1);
+        list2.add(2);
+
+        
+        System.out.println(list.contains(30));
+        list.addAll(list2);
         System.out.println(list);
+
     }
     
 }
